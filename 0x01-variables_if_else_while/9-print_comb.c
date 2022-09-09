1f29423aafbfc6f2c063be207b2c8e3ef560d76e
Kenny15200 <opaleyekehinde70@gmail.com>
2022-09-09 04:37:48 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+/**
+ *
+ *  * main - takes unspecificed number of arguments and returns value on
+ *
+ *   * Description: longer things about the main function
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int num;
+
+
+
+			for (num = '0'; num <= '9'; num++)
+
+					{
+
+								if (num >= '0' && num <= '9')
+
+											{
+
+															putchar(num);
+
+																	}
+
+										if (num >= '0' && num < '9')
+
+													{
+
+																	putchar(',');
+
+																				putchar(' ');
+
+																						}
+
+											}
+
+				putchar('\n');
+
+					return (0);
+
+}
