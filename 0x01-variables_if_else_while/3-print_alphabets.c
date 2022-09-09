dc6e8b1f0c71529e23222d705152333a4ad1bf0a
Kenny15200 <opaleyekehinde70@gmail.com>
2022-09-09 03:20:02 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,41 @@
+
+
+/** this is a short description for a program to print plain text/file size */
+
+#include<stdio.h>
+
+/**
+ *
+ *  * main - takes unspecificed number of arguments and returns value on
+ *
+ *   * Description: longer things about the main function
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int al;
+
+			int am;
+
+
+
+				for (al = 'a'; al <= 'z'; al++)
+
+							putchar(al);
+
+					for (am = 'A'; am <= 'Z'; am++)
+
+								putchar(am);
+
+						putchar('\n');
+
+
+
+							return (0);
+
+}
