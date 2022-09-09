fa3b29129063fde9308f31acc62f456a1e5085c2
Kenny15200 <opaleyekehinde70@gmail.com>
2022-09-09 03:52:25 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,37 @@
+/** this is a short description for a program to print plain text/file size */
+
+#include<stdio.h>
+
+/**
+ *
+ *  * main - takes unspecificed number of arguments and returns value on
+ *
+ *   * Description: longer things about the main function
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int al;
+
+
+
+			for (al = 122; al >= 97; --al)
+
+					{
+
+								putchar(al);
+
+									}
+
+				putchar('\n');
+
+
+
+					return (0);
+
+}
