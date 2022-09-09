74f2d9c05f23a581c13f3e7f8e66b97caac3eab9
Kenny15200 <opaleyekehinde70@gmail.com>
2022-09-09 14:49:41 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,53 +1,21 @@
-/** this is a short description for a program to print plain text/file size */
-
-#include<stdio.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
  *
- *  * main - takes unspecificed number of arguments and returns value on
- *
- *   * Description: longer things about the main function
- *
- *    * Return: 0
- *
- *     */
+ *  Return: 0
+ */
 
 int main(void)
-
 {
-
-		char c;
-
-			int i;
-
-				double d;
-
-					float f;
-
-
-
-						printf("Size of a char: %lu", (unsigned long)sizeof(c));
-
-							printf(" byte(s)\n");
-
-								printf("Size of an int: %lu", (unsigned long)sizeof(i));
-
-									printf(" byte(s)\n");
-
-										printf("Size of a long int: %lu", (unsigned long)sizeof(i));
-
-											printf(" byte(s)\n");
-
-												printf("Size of a long long int: %lu", (unsigned long)sizeof(d));
-
-													printf(" byte(s)\n");
-
-														printf("Size of a float: %lu", (unsigned long)sizeof(f));
-
-															printf(" byte(s)\n");
-
-
-
-																return (0);
-
+	int l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l += 1;
+	}
+	putchar('\n');
+	return (0);
 }
