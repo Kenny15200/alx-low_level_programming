a9b5715594e33e398b1edb8904ccf221f5d08a67
Kenny15200 <opaleyekehinde70@gmail.com>
2022-09-09 17:34:54 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
-#include <ctype.h>
-
 /**
- * main - program that prints the alphabet in lowercase, followed by a new line
- * You can only use the putchar
- *
- *  Return: 0
+ * main - program that prints all possible combinations of two two-digit
+ * The numbers should range from 0 to 99
+ * Numbers must be separated by ,followed by a space
+ * All numbers should be printed with two digits. 1 should be printed as 01
+ * Return: 0
  */
 
 int main(void)
+
 {
-	int l = 'a';
-	while (l <= 'z')
+	int n1 = 48;
+	int a = 0;
+	int b;
+	int com = 44;
+
+	while (a <= 99)
 	{
-		putchar(l);
-		l += 1;
+		b = a + 1;
+
+		while (b <= 99)
+		{
+			putchar((a / 10) + n1);
+			putchar((a % 10) + n1);
+			putchar(32);
+			putchar((b / 10) + n1);
+			putchar((b % 10) + n1);
+			if (a != 98 || b != 99)
+			{
+				putchar(com);
+				putchar(32);
+			}
+			b += 1;
+		}
+		b += 1;
 	}
 	putchar('\n');
 	return (0);
