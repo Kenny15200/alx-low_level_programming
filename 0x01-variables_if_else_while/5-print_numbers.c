80e202a21c858ec3f143c02db7f3690723aa16d6
Kenny15200 <opaleyekehinde70@gmail.com>
2022-09-09 21:08:58 -0700

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,33 +1,18 @@
-/** this is a short description for a program to print plain text/file size */
-
 #include<stdio.h>
-
 /**
- *
- *  * main - takes unspecificed number of arguments and returns value on
- *
- *   * Description: longer things about the main function
- *
- *    * Return: 0
- *
- *     */
+ * main - takes unspecificed number of arguments and returns value on
+ * Description: longer things about the main function
+ * Return: 0
+ */
 
 int main(void)
-
 {
+	int al;
 
-		int al;
+	for (al = '0'; al <= '9'; al++)
 
+		putchar(al);
+	putchar('\n');
 
-
-			for (al = '0'; al <= '9'; al++)
-
-						putchar(al);
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
