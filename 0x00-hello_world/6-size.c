119cb85636f4d43b7f81a49a8a4277233dd81664
Kenny15200 <opaleyekehinde70@gmail.com>
2022-09-12 19:21:56 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints the sizeof many var types
- * Description: longer things about the main functionlonger things about the main function
+ * Description: longer things about the main functionlonger things about the mai
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,10 +13,10 @@ int main(void)
 	char d;
 	float f;
 
-	puts("Size of a char: %d", (unsigned long)sizeof(d));
-	puts("Size of an int: %d", (unsigned long)sizeof(a));
-	puts("Size of a long int: %d", (unsigned long)sizeof(b));
-	puts("Size of a long long int: %d", (unsigned long)sizeof(c));
-	puts("Size of a float: %d", (unsigned long)sizeof(f);
+	puts("Size of a char: %d\n", (unsigned long)sizeof(d));
+	puts("Size of an int: %d\n", (unsigned long)sizeof(a));
+	puts("Size of a long int: %d\n", (unsigned long)sizeof(b));
+	puts("Size of a long long int: %d\n", (unsigned long)sizeof(c));
+	puts("Size of a float: %d\n", (unsigned long)sizeof(f);
 	return (0);
 }
