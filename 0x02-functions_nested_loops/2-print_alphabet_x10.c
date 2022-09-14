34be692b5cd562c66f5018222055c621227bf950
Kenny15200 <opaleyekehinde70@gmail.com>
2022-09-14 08:59:33 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - takes unspecificed number of arguments, returns value
+ * Description: longer things about the print_alphabet_x10 function
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int num;
+	int al;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (al = 'a'; al <= 'z'; al++)
+		{
+			_putchar(al);
+		}
+		_putchar('\n');
+
+	}
+
+}
+
