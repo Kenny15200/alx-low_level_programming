32940008e82cb5396ed8f6703aee80aa0b976ac4
Kenny15200 <111121615+Kenny15200@users.noreply.github.com>
2022-10-11 23:04:07 +0100

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,30 @@
-#include "dog.h"
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: param 1
- * @name: param 2
- * @age: param 3
- * @owner: param 4
- * Return: nothing
+ * dog_t - Typedef for struct dog
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: oner of the dog
+ *
+ * Description: struct called that stores the above param
+ */
+struct dog
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif
