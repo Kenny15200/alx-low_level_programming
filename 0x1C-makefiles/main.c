cbf2d28f165ce15a21eaedc9dfd02af9ffaea77e
Kenny15200 <opaleyekehinde70@gmail.com>
2023-02-25 18:34:42 +0100

    3-Makefile

--- 0x1C-makefiles/main.c
+++ 0x1C-makefiles/main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,17 +11,8 @@
 int main(void)
 {
 	printf("print_school");
-=======
-#include "m.h"
-
-/**
- * main - Entry point
- *
- * Return: Always 
- */
-int main(void)
-{
-	print_school();
->>>>>>> 048b64bc37dc13daf5824a988082f2e4d9fa9b06
 	return (EXIT_SUCCESS);
+
 }
+/* School */
+/* School */
